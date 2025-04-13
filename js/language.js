a81d9b658b71a50b9473b4677be5c48256e5164e
Ravikumar <rvkmar@gmail.com>
2025-04-13 21:56:10 +0530

    language slider added

---
+++ js/language.js
@@ -0,0 +1,10 @@
+// // language toggle
+//   document.getElementById('langToggle').addEventListener('change', function () {
+//     if (this.checked) {
+//       // Tamil selected
+//       window.location.href = "/ta"; // or trigger Tamil content
+//     } else {
+//       // English selected
+//       window.location.href = "/en"; // or trigger English content
+//     }
+//   });
